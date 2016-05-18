9b52e52cab93a102f9fbadc289908f485743cd99
Daniel Kindimba <daniellykindimba@gmail.com>
2016-05-18 06:43:15 +0300

    dafdfads

---
+++ app/migrations/0047_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0045_sdrive_size'),
+        ('app', '0046_auto_20160517_2310'),
+    ]
+
+    operations = [
+    ]
